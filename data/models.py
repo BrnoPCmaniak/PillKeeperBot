c112e4564abcc6d97f3617cd37bb62ccf5908350
Filip Dobrovolný <brnopcman@gmail.com>
2016-06-06 14:04:42 +0200

    ADDED: models

--- data/models.py
+++ data/models.py
@@ -5,3 +5,24 @@ try:
 except  Exception:
     print("There was an error loading django modules. Do you have django installed?")
     sys.exit()
+
+class Pill(models.Model):
+    chat_id = models.BigIntegerField()
+    user_id = models.BigIntegerField()
+    name = models.CharField(max_length=60)
+
+class WeekDaysRepetion(models.Model):
+    pill = models.ForeignKey(Pill)
+    day = models.IntegerField()
+
+class RepeatEveryDay(models.Model):
+    pill = models.OneToOneField(Pill)
+
+class RepeatEveryNthDay(models.Model):
+    pill = models.OneToOneField(Pill)
+    n = models.IntegerField()
+
+class Time(models.Model):
+    pill = models.ForeignKey(Pill)
+    time = models.TimeField()
+    last_done = models.DateField(auto_now=True)
