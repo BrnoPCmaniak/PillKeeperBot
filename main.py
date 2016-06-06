d098c2dc0c889c1fc370f22b1d79512a941359b0
Filip Dobrovolný <brnopcman@gmail.com>
2016-06-06 16:16:32 +0200

    UPDATED: now using db

--- main.py
+++ main.py
@@ -27,66 +27,47 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - '
     MENU, ENTER_NAME, ENTER_TIMES, CHOOSE_REPEATING, ENTER_N_DAYS,
     ENTER_DATES) = range(6)
 # Define choices for inline choose of repeating
-EVERY_DAY, EVERY_N_DAY, WEEK_DAYS = [str(i) for i in range(3)]
-# Define choices for work day select
-(
-    SUNDAY_ON, SUNDAY_OFF, MONDAY_ON, MODAY_OFF, TUESDAY_ON, TUESDAY_OFF,
-    WEDNESDAY_ON, WEDNESDAY_OFF, THURSDAY_ON, THURSDAY_OFF, FRIDAY_ON,
-    FRIDAY_OFF, SATURDAY_ON,
-    SATURDAY_OFF, SAVE) = [str(i) for i in range(3, 18)]
+EVERY_DAY, EVERY_N_DAY, WEEK_DAYS, SAVE = [str(i) for i in range(4)]
 
-# States are saved in a dict that maps chat_id -> state
-state = dict()
-# Sometimes you need to save data temporarily
-context = dict()
-days = dict()
-# This dict is used to store the settings value for the chat.
-# Usually, you'd use persistence for this (e.g. sqlite).
-values = dict()
-# Stack of last confirmations
-confirmations = dict()
+DAYS = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
 
+def gen_reply_week_day_markup(user_state):
+    """
+    example:
+    [[InlineKeyboardButton("Sun - OFF",
+                           callback_data=SUNDAY_ON),
+      InlineKeyboardButton("Mon - OFF",
+                           callback_data=MONDAY_ON),
+      InlineKeyboardButton("Tue - OFF",
+                           callback_data=TUESDAY_ON)],
+     [InlineKeyboardButton("Wed - OFF",
+                           callback_data=WEDNESDAY_ON),
+      InlineKeyboardButton("Thu - OFF",
+                           callback_data=THURSDAY_ON),
+      InlineKeyboardButton("Fri - OFF",
+                           callback_data=FRIDAY_ON)],
+     [InlineKeyboardButton("Sat - OFF",
+                           callback_data=SATURDAY_ON),
+      InlineKeyboardButton("Save", callback_data=SAVE)]])
+    """
+    def get_day(pill, day):
+        try:
+            WeekDaysRepetion.objects.get(pill=pill, day=day)
+            found = True
+        except WeekDaysRepetion.DoesNotExist:
+            found = False
+        found_str = " - ON" if found else " - OFF"
+        found_int = "0" if found else "1"
+        print(str(day), found_int.__class__, DAYS[day].__class__,found_str.__class__)
+        return InlineKeyboardButton(DAYS[day] + found_str,
+                                    callback_data=str(day) + found_int)
 
-class Pill(object):
-    times = None
-    name = None
-    user_id = None
-    repeatEveryDay = False
-    repeatEveryNthDay = None
-    repeatDaysOfWeek = None
+    p = user_state.pill
 
-    def __init__(self, name, user_id):
-        self.times = []
-        self.user_id = user_id
-        self.name = name
-
-    def add_time(self, hour, minute):
-        hour_i, minute_i = int(hour), int(minute)
-        logging.debug("adding to \"%s\" time %d:%d" % (self.name, hour_i,
-                                                       minute_i))
-        self.times.append((hour_i, minute_i))
-
-    def add_time_from_re_match(self, match):
-        self.add_time(*match.group(1, 2))
-
-    def __str__(self):
-        return self.name + " " + str(self.times)
-
-
-def add_and_return_context(name, user_id):
-    p = Pill(name, user_id)
-    context[user_id] = p
-    return p
-
-
-def save(user_id, p):
-    if user_id not in values:
-        values[user_id] = []
-    values[user_id].append(p)
-    del state[user_id]
-    del context[user_id]
-    logging.info("SAVED: %s" % str(p))
-
+    return InlineKeyboardMarkup(
+        [[get_day(p, 6), get_day(p, 0), get_day(p, 1)],
+         [get_day(p, 2), get_day(p, 3), get_day(p, 4)],
+         [get_day(p, 5), InlineKeyboardButton("Save", callback_data=SAVE)]])
 
 def start(bot, update):
     bot.sendMessage(update.message.chat_id,
@@ -109,21 +90,23 @@ def new_pill(bot, update):
 def entered_value(bot, update):
     chat_id = update.message.chat_id
     user_id = update.message.from_user.id
-    chat_state = state.get_state(user_id, MENU)
+    user_state = State.get_state(chat_id, user_id)
 
     # Check if we are waiting for input
-    if chat_state == ENTER_NAME:
-        state[user_id] = ENTER_TIMES
+    if user_state.state == ENTER_NAME:
+        user_state.state = ENTER_TIMES
 
         # Save the user id and the answer to context
-        pill = add_and_return_context(update.message.text, user_id)
+        pill = Pill(name=update.message.text, user_id=user_id, chat_id=chat_id)
+        pill.save()
+        user_state.pill = pill
+        user_state.save()
         bot.sendMessage(chat_id,
                         text="Please enter time/s in format 12:00 (GMT)")
-    elif chat_state == ENTER_TIMES:
+    elif user_state.state == ENTER_TIMES:
         m = re.match(r"^(\d{1,2}):(\d{1,2})$", update.message.text)
         if m is not None:
-            pill = context.get(user_id, None)
-            pill.add_time_from_re_match(m)
+            user_state.pill.add_time_from_re_match(m)
             bot.sendMessage(chat_id,
                             text="Got {0}:{1}\nPlease enter another time/s in "
                                  "format 12:00 (GMT) or enter "
@@ -133,12 +116,11 @@ def entered_value(bot, update):
                             text="Invalid format please try again.\n"
                                  "Format must be hh:mm or h:mm or h:m in "
                                  "24h format!")
-    elif chat_state == ENTER_N_DAYS:
+    elif user_state.state == ENTER_N_DAYS:
         m = re.match(r"^(\d+)$", update.message.text)
         if m is not None:
-            pill = context.get(user_id, None)
-            pill.repeatEveryNthDay = int(m.group(1))
-            save(user_id, pill)
+            RepeatEveryNthDay(pill=user_state.pill, n=int(m.group(1))).save()
+            user_state.clean()
             bot.sendMessage(
                 chat_id,
                 text="Finished. Annoucement will repeat every "
@@ -152,11 +134,11 @@ def entered_value(bot, update):
 def next_state(bot, update):
     chat_id = update.message.chat_id
     user_id = update.message.from_user.id
-    chat_state = state.get(user_id, MENU)
-    if chat_state == ENTER_TIMES:
-        pill = context.get(user_id, None)
-        if len(pill.times) != 0:
-            state[user_id] = CHOOSE_REPEATING
+    user_state = State.get_state(chat_id, user_id)
+    if user_state.state == ENTER_TIMES:
+        if user_state.pill.times.count() != 0:
+            user_state.state = CHOOSE_REPEATING
+            user_state.save()
             reply_markup = InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Every day", callback_data=EVERY_DAY)],
                  [InlineKeyboardButton("Every nth day",
@@ -177,42 +159,52 @@ def callback_handler(bot, update):
     chat_id = query.message.chat_id
     user_id = query.from_user.id
     text = query.data
-    user_state = state.get(user_id, MENU)
-    user_context = context.get(user_id, None)
+    user_state = State.get_state(chat_id, user_id)
 
     # Check if we are waiting for confirmation and the right user answered
-    if user_state == CHOOSE_REPEATING:
+    if user_state.state == CHOOSE_REPEATING:
         if text == EVERY_DAY:
-            user_context.repeatEveryDay = True
-            save(user_id, user_context)
+            RepeatEveryDay(pill=user_state.pill).save()
+            user_state.clean()
             bot.editMessageText(
                 text="Finished. Repeating annoucement every day.",
                 chat_id=chat_id,
                 message_id=query.message.message_id)
         elif text == EVERY_N_DAY:
-            state[user_id] = ENTER_N_DAYS
+            user_state.state = ENTER_N_DAYS
+            user_state.save()
             bot.editMessageText(text="Please enter number of days:",
                                 chat_id=chat_id,
                                 message_id=query.message.message_id)
         elif text == WEEK_DAYS:
-            reply_markup = InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Sun - OFF",
-                                       callback_data=SUNDAY_ON),
-                  InlineKeyboardButton("Mon - OFF",
-                                       callback_data=MONDAY_ON),
-                  InlineKeyboardButton("Tue - OFF",
-                                       callback_data=TUESDAY_ON)],
-                 [InlineKeyboardButton("Wed - OFF",
-                                       callback_data=WEDNESDAY_ON),
-                  InlineKeyboardButton("Thu - OFF",
-                                       callback_data=THURSDAY_ON),
-                  InlineKeyboardButton("Fri - OFF",
-                                       callback_data=FRIDAY_ON)],
-                 [InlineKeyboardButton("Sat - OFF",
-                                       callback_data=SATURDAY_ON),
-                  InlineKeyboardButton("Save", callback_data=SAVE)]])
+            user_state.state = ENTER_DATES
+            user_state.save()
+            reply_markup = gen_reply_week_day_markup(user_state)
             bot.editMessageText(
                 text="Choose whcih days you want to be annouced.",
+                chat_id=chat_id,
+                message_id=query.message.message_id,
+                reply_markup=reply_markup)
+    elif user_state.state == ENTER_DATES:
+        if text == SAVE:
+            user_state.clean()
+            bot.editMessageText(
+                text="Finished.",
+                chat_id=chat_id,
+                message_id=query.message.message_id)
+        else:
+            try:
+                if int(text[1]):  # create data
+                    WeekDaysRepetion(pill=user_state.pill,
+                                     day=int(text[0])).save()
+                else:
+                    WeekDaysRepetion.objects.get(pill=user_state.pill,
+                                                 day=int(text[0])).remove()
+            except:
+                pass
+            reply_markup = gen_reply_week_day_markup(user_state)
+            bot.editMessageText(
+                text="Choose which days you want to be annouced.",
                 chat_id=chat_id,
                 message_id=query.message.message_id,
                 reply_markup=reply_markup)
@@ -232,12 +224,11 @@ def list_pills(bot, update):
     chat_id = update.message.chat_id
     user_id = update.message.from_user.id
 
-    logging.warn(str(chat_id) + ":!:" + str(user_id))
-
+    pills = Pill.objects.filter(chat_id=chat_id, user_id=user_id)
     bot.sendMessage(
         chat_id,
         text="List:%s" % (
-            "".join(["\n * " + str(i) for i in values.get(user_id, [])]))
+            "".join(["\n * " + str(i.name) for i in pills]))
         )
 
 updater = Updater(os.environ.get('TOKEN'))
